642448c021711b3a2ab937fd5c3360b5aaf6db6b
zxy <zhangxycg@126.com>
2019-11-23 12:45:33 +0800

    从零开始搭建mybatis环境，查询数据库信息

---
+++ src/main/java/com/zxy/dao/IUserDao.java
@@ -0,0 +1,19 @@
+package com.zxy.dao;
+
+import com.zxy.pojo.User;
+
+import java.util.List;
+
+/**
+ * @Description: 用户持久层接口
+ * @Author: zhangxy
+ * @Date: Created in 2019/11/22
+ * @Modified By:
+ */
+public interface IUserDao {
+    /**
+     * 查询所有信息
+     * @return
+     */
+    List<User> findAll();
+}
